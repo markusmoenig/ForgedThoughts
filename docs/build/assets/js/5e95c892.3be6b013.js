296fe995162c4f4a3cd30650a3b115e282d834be
Markus Moenig <markus@moenig.io>
2026-03-11 14:25:38 +0700

    Delete build dir

--- docs/build/assets/js/5e95c892.3be6b013.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkforgedthoughts_docs=self.webpackChunkforgedthoughts_docs||[]).push([[647],{7121(e,s,r){r.r(s),r.d(s,{default:()=>a});r(6540);var t=r(4164),u=r(7559),c=r(5500),o=r(2831),d=r(1656),h=r(4848);function a(e){return(0,h.jsx)(c.e3,{className:(0,t.A)(u.G.wrapper.docsPages),children:(0,h.jsx)(d.A,{children:(0,o.v)(e.route.routes)})})}}}]);
--- docs/build/assets/js/a7456010.c9b29c30.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkforgedthoughts_docs=self.webpackChunkforgedthoughts_docs||[]).push([[235],{8552(s){s.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
